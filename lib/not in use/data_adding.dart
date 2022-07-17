ddcaa95433e71e3137fbd427c0dc2491a69ca1d9
SiddhantKumar <siddhant0123456@gmail.com>
2022-07-17 23:25:05 +0530

    changes to data ading screen

--- lib/not in use/data_adding.dart
+++ lib/not in use/data_adding.dart
@@ -10,69 +10,64 @@ class AddingData extends StatelessWidget {
   TextEditingController image = TextEditingController();
   TextEditingController extra = TextEditingController();
 
+  AddingData({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Container(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: name,
-                    decoration: InputDecoration(hintText: 'name'),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: rank,
-                    decoration: InputDecoration(hintText: 'rank'),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: description,
-                    decoration: InputDecoration(hintText: 'description'),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: height,
-                    decoration: InputDecoration(hintText: 'height'),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: history,
-                    decoration: InputDecoration(hintText: 'histroy'),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: image,
-                    decoration: InputDecoration(hintText: 'image'),
-                  ),
-                  TextFormField(
-                    controller: extra,
-                    decoration: InputDecoration(hintText: 'extra'),
-                  ),
-                  SizedBox(height: 10),
-                  FlatButton(
-                      onPressed: () {
-                        Map<String, dynamic> data = {
-                          'date': DateTime.now(),
-                          'name': name.text.trim(),
-                          'rank': rank.text.trim(),
-                          'description': description.text.trim(),
-                          'height': height.text.trim(),
-                          'history': history.text.trim(),
-                          'image': image.text.trim(),
-                          'extra': extra.text.trim(),
-                        };
-                        FirebaseFirestore.instance
-                            .collection('characters')
-                            .add(data);
-                      },
-                      child: Text('submit')),
-                ],
-              ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextFormField(
+                  controller: name,
+                  decoration: const InputDecoration(hintText: 'name'),
+                ),
+                TextFormField(
+                  controller: rank,
+                  decoration: const InputDecoration(hintText: 'rank'),
+                ),
+                TextFormField(
+                  controller: description,
+                  decoration: const InputDecoration(hintText: 'description'),
+                ),
+                TextFormField(
+                  controller: height,
+                  decoration: const InputDecoration(hintText: 'height'),
+                ),
+                TextFormField(
+                  controller: history,
+                  decoration: const InputDecoration(hintText: 'histroy'),
+                ),
+                TextFormField(
+                  controller: image,
+                  decoration: const InputDecoration(hintText: 'image'),
+                ),
+                TextFormField(
+                  controller: extra,
+                  decoration: const InputDecoration(hintText: 'extra'),
+                ),
+                FlatButton(
+                    onPressed: () {
+                      Map<String, dynamic> data = {
+                        'date': DateTime.now(),
+                        'name': name.text.trim(),
+                        'rank': rank.text.trim(),
+                        'description': description.text.trim(),
+                        'height': height.text.trim(),
+                        'history': history.text.trim(),
+                        'image': image.text.trim(),
+                        'extra': extra.text.trim(),
+                      };
+                      FirebaseFirestore.instance
+                          .collection('characters')
+                          .add(data);
+                    },
+                    child: const Text('submit')),
+              ],
             ),
           ),
         ));
